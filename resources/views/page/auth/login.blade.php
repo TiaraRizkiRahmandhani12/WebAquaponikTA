ed6f0123a2e1016a8e82dc4f95aa78a16518506a
SalsabilaFatmasari <salsabilafatmasari@gmail.com>
2024-04-22 22:09:37 +0700

    login

--- resources/views/page/auth/login.blade.php
+++ resources/views/page/auth/login.blade.php
@@ -10,45 +10,76 @@
     <style>
         body {
             background-color: #f4f4f4;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .login-container {
+            width: calc(100% - 100px);
+            /* Lebar device dikurangi 100px */
+            margin-left: 100px;
+            /* Margin kiri 100px */
+            max-width: 400px;
+            /* Maksimum lebar kotak */
+        }
+
+        .card {
+            border-radius: 15px;
+            box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.1);
         }
 
         .login-form {
-            width: 300px;
-            margin: 100px auto;
             padding: 30px;
-            background: #fff;
-            border-radius: 5px;
-            box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.1);
+        }
+
+        .card-button {
+            margin-top: 20px;
+            text-align: center;
         }
     </style>
 </head>
 
 <body>
 
-    <div class="login-form">
-        <h2 class="text-center mb-4">Login</h2>
-        <form action="{{ route('login.process') }}" method="post">
-            @csrf
-            <div class="form-group">
-                <input type="text" class="form-control" name="username" placeholder="Username" required="required">
+    <div class="login-container">
+        {{-- <div class="card">
+            <div class="card-body login-form">
+                <h2 class="text-center mb-4">Login</h2>
+                <form action="{{ route('login.process') }}" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="username" placeholder="Username"
+                            required="required">
+                    </div>
+                    @error('username')
+                        <small>{{ $message }}</small>
+                    @enderror
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="password" xplaceholder="Password"
+                            required="required">
+                    </div>
+                    @error('username')
+                        <small>{{ $message }}</small>
+                    @enderror
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
+                    </div>
+                    <div class="clearfix">
+                        <label class="float-left form-check-label"><input type="checkbox"> Remember me</label>
+                        <a href="#" class="float-right">Forgot Password?</a>
+                    </div>
+                </form>
             </div>
-            @error('username')
-                <small>{{ $message }}</small>
-            @enderror
-            <div class="form-group">
-                <input type="password" class="form-control" name="password" xplaceholder="Password" required="required">
+        </div> --}}
+
+        <!-- New Card with Button -->
+        <div class="card card-button">
+            <div class="card-body">
+                <button class="btn btn-secondary btn-block">Create New Account</button>
             </div>
-            @error('username')
-                <small>{{ $message }}</small>
-            @enderror
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-block">Login</button>
-            </div>
-            <div class="clearfix">
-                <label class="float-left form-check-label"><input type="checkbox"> Remember me</label>
-                <a href="#" class="float-right">Forgot Password?</a>
-            </div>
-        </form>
+        </div>
     </div>
 
     <!-- Bootstrap JS -->
